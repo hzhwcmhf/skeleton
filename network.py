d1b8fa4251936fb49936efa4bdec0765989a0424
hzhwcmhf <hzhwcmhf@gmail.com>
2019-03-17 19:55:19 +0800

    . sync at 20190317 195519

--- network.py
+++ network.py
@@ -82,7 +82,7 @@ class GenNetwork(nn.Module):
 		EOSmet = []
 
 		next_emb = first_emb
-		gru_h = self.GRULayer.getInitialParameter(batch_size)
+		gru_h = self.GRULayer.getInitialParameter(batch_size)[0]
 		for _ in range(self.args.max_sen_length):
 			now = next_emb
 			gru_h = self.GRULayer.cell_forward(now, gru_h)
